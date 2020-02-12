67d5a3ffd6468ba15c9314564d082d1a04bea26e
ABDO-ALT <abdomohamad009639@gmail.com>
2020-02-12 21:13:31 +0100

    i am trying to add more objects to the array but its not work

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+
 
 const albumsData = [
   {
@@ -30,12 +33,26 @@ app.get("/albums", function(req, res) {
 });
 app.get("/albums/:albumId", function(req, res) {
     const albumId = req.params.albumId
-    const album = albums...
+    const album = albums.push({
+      albumId: "13",
+  artistName: "Beyoncé",
+  collectionName: "B'Day (Deluxe Edition)",
+  artworkUrl100: "http://is5.mzstatic.com/image/thumb/Music/v4/6c/fc/6a/6cfc6a13-0633-f96b-9d72-cf56774beb4b/source/100x100bb.jpg",
+  releaseDate: "2007-05-29T07:00:00Z",
+  primaryGenreName: "Pop",
+  url: "https://www.youtube.com/embed/RQ9BWndKEgs?rel=0&amp;controls=0&amp;showinfo=0"
+    })
     res.send(album)
+    console.log(albums)
 });
+
 app.post("/albums", function(req, res) {
-  console.log("POST /albums route");
+
+  console.log()
 });
+
+app.use(bodyParser.json());
+
 app.listen(3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
