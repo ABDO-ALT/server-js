f816d7066b1a8df50bb8732775f32dfa41d083f4
ABDO-ALT <abdomohamad009639@gmail.com>
2020-02-18 18:21:34 +0100

    done

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
-
+app.use(bodyParser.json());
 
 const albumsData = [
   {
     albumId: "10",
     artistName: "Beyoncé",
     collectionName: "Lemonade",
-    artworkUrl100:
+    sartworkUrl100:
       "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
     releaseDate: "2016-04-25T07:00:00Z",
     primaryGenreName: "Pop",
@@ -28,30 +27,34 @@ const albumsData = [
       "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0"
   }
 ];
+
 app.get("/albums", function(req, res) {
-  res.send(albumsData); 
+  res.send(albumsData);
 });
 app.get("/albums/:albumId", function(req, res) {
-    const albumId = req.params.albumId
-    const album = albums.push({
-      albumId: "13",
-  artistName: "Beyoncé",
-  collectionName: "B'Day (Deluxe Edition)",
-  artworkUrl100: "http://is5.mzstatic.com/image/thumb/Music/v4/6c/fc/6a/6cfc6a13-0633-f96b-9d72-cf56774beb4b/source/100x100bb.jpg",
-  releaseDate: "2007-05-29T07:00:00Z",
-  primaryGenreName: "Pop",
-  url: "https://www.youtube.com/embed/RQ9BWndKEgs?rel=0&amp;controls=0&amp;showinfo=0"
-    })
-    res.send(album)
-    console.log(albums)
-});
 
-app.post("/albums", function(req, res) {
+  res.send(req.params.albumId)
 
+  // albumID = 10
+  const albumId = req.params.albumId;
+
+  const album = albumsData.find(function (item) {
+    return item.albumId === albumId
+  })
+
+  res.send(album);
   console.log()
 });
 
-app.use(bodyParser.json());
+app.post("/albums", function(req, res) {
+  console.log(req.body)
+
+  albumsData.push(req.body)
+
+  res.send()
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests!");
